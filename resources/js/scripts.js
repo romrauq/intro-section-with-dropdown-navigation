b1e30d7f005ab93de24693eee528cd3872378b2c
romrauq <johnequarmor@gmail.com>
2025-02-02 10:53:18 +0000

    Prepared starter script for side menu buttons.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,6 +2,8 @@ const features_link = document.getElementById("features-link");
 const company_link = document.getElementById("company-link");
 const features_dropdown = document.getElementById("features-dropdown-container");
 const company_dropdown = document.getElementById("company-dropdown-container");
+const burger_menu_button = document.getElementById("burger-menu-button");
+const close_menu_button = document.getElementById("close-menu-button");
 
 // Display features dropdown container when button text is clsiked:
 features_link.addEventListener("click", (event) => {
@@ -27,4 +29,14 @@ document.addEventListener("click", (event) => {
 	if (!company_dropdown.contains(event.target) && event.target !== company_link) {
 		company_dropdown.style.display = "none";
 	}
+});
+
+burger_menu_button.addEventListener("click", () => {
+	burger_menu_button.style.display = "none";
+	close_menu_button.style.display = "block";
+});
+
+close_menu_button.addEventListener("click", () => {
+	close_menu_button.style.display = "none";
+	burger_menu_button.style.display = "block";
 });
