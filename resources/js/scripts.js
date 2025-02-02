9a374bda380f183f6b2e67a0b27eaf500d2d0952
romrauq <johnequarmor@gmail.com>
2025-02-02 10:25:01 +0000

    Prepared script that displays & removed dropdown container elements using user click events.

---
+++ resources/js/scripts.js
@@ -0,0 +1,30 @@
+const features_link = document.getElementById("features-link");
+const company_link = document.getElementById("company-link");
+const features_dropdown = document.getElementById("features-dropdown-container");
+const company_dropdown = document.getElementById("company-dropdown-container");
+
+// Display features dropdown container when button text is clsiked:
+features_link.addEventListener("click", (event) => {
+	event.preventDefault();
+	features_dropdown.style.display = "flex";
+});
+
+// Remove features dropdown container when user clicks outside of dropdown container:
+document.addEventListener("click", (event) => {
+	if (!features_dropdown.contains(event.target) && event.target !== features_link) {
+		features_dropdown.style.display = "none";
+	}
+});
+
+// Display company dropdown container when button text is clsiked:
+company_link.addEventListener("click", (event) => {
+	event.preventDefault();
+	company_dropdown.style.display = "flex";
+});
+
+// Remove company dropdown container when user clicks outside of dropdown container:
+document.addEventListener("click", (event) => {
+	if (!company_dropdown.contains(event.target) && event.target !== company_link) {
+		company_dropdown.style.display = "none";
+	}
+});
