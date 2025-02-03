db1b78d2a038423397bd0e85ddf8a77440238dae
romrauq <johnequarmor@gmail.com>
2025-02-03 20:21:23 +0000

    Added scripts to toggle dropdown arroes when respective buttons are clicked.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,6 +1,10 @@
 const overlay = document.getElementById("overlay");
 const features_link = document.getElementById("features-link");
 const company_link = document.getElementById("company-link");
+const features_arrow_down = document.getElementById("features-arrow-down");
+const features_arrow_up = document.getElementById("features-arrow-up");
+const company_arrow_down = document.getElementById("company-arrow-down");
+const company_arrow_up = document.getElementById("company-arrow-up");
 const features_dropdown = document.getElementById("features-dropdown-container");
 const company_dropdown = document.getElementById("company-dropdown-container");
 const burger_menu_button = document.getElementById("burger-menu-button");
@@ -11,6 +15,8 @@ const side_menu_container = document.getElementById("side-menu-container");
 features_link.addEventListener("click", (event) => {
 	event.preventDefault();
 	features_dropdown.style.display = "flex";
+	features_arrow_down.style.display = "none";
+	features_arrow_up.style.display = "block";
 });
 
 // Remove features dropdown container when user clicks outside of dropdown container:
@@ -18,6 +24,8 @@ document.addEventListener("click", (event) => {
 	event.preventDefault();
 	if (!features_dropdown.contains(event.target) && event.target !== features_link) {
 		features_dropdown.style.display = "none";
+		features_arrow_down.style.display = "block";
+		features_arrow_up.style.display = "none";
 	}
 });
 
@@ -25,6 +33,8 @@ document.addEventListener("click", (event) => {
 company_link.addEventListener("click", (event) => {
 	event.preventDefault();
 	company_dropdown.style.display = "flex";
+	company_arrow_down.style.display = "none";
+	company_arrow_up.style.display = "block";
 });
 
 // Remove company dropdown container when user clicks outside of dropdown container:
@@ -32,6 +42,8 @@ document.addEventListener("click", (event) => {
 	event.preventDefault();
 	if (!company_dropdown.contains(event.target) && event.target !== company_link) {
 		company_dropdown.style.display = "none";
+		company_arrow_down.style.display = "block";
+		company_arrow_up.style.display = "none";
 	}
 });
 
