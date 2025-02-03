1238c127397c0ff94d91a1836c21adfe8cf7dfff
romrauq <johnequarmor@gmail.com>
2025-02-03 19:02:41 +0000

    Set side menu to appear/disappear when burger and close buttons are clicked resopectively

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,11 +1,13 @@
+const overlay = document.getElementById("overlay");
 const features_link = document.getElementById("features-link");
 const company_link = document.getElementById("company-link");
 const features_dropdown = document.getElementById("features-dropdown-container");
 const company_dropdown = document.getElementById("company-dropdown-container");
 const burger_menu_button = document.getElementById("burger-menu-button");
 const close_menu_button = document.getElementById("close-menu-button");
+const side_menu_container = document.getElementById("side-menu-container");
 
-// Display features dropdown container when button text is clsiked:
+// Display features dropdown container when button text is clicked:
 features_link.addEventListener("click", (event) => {
 	event.preventDefault();
 	features_dropdown.style.display = "flex";
@@ -34,9 +36,13 @@ document.addEventListener("click", (event) => {
 burger_menu_button.addEventListener("click", () => {
 	burger_menu_button.style.display = "none";
 	close_menu_button.style.display = "block";
+	overlay.style.display = "block";
+	side_menu_container.style.right = "0";
 });
 
 close_menu_button.addEventListener("click", () => {
 	close_menu_button.style.display = "none";
 	burger_menu_button.style.display = "block";
+	overlay.style.display = "none";
+	side_menu_container.style.right = "-70%";
 });
